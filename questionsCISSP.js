e8fc3fce24eca7755ee9ef803fc3cfbe26711abe
nbavaro1 <39228701+nbavaro1@users.noreply.github.com>
2023-08-23 12:49:47 -0400

    Create questionsCISSP.js

---
+++ questionsCISSP.js
@@ -0,0 +1,18 @@
+var questions = [
+  {
+      "question": "The following are characteristics of a project except?",
+      "option1": "It is a temporary in nature",
+      "option2": "It is continuous",
+      "option3": "It is unique",
+      "option4": "It has a definitive end",
+      "answer": 2
+  },
+
+  {
+      "question": "When is a project considered successful?",
+      "option1": "All deliverables have been completed",
+      "option2": "The phase completion has been approved",
+      "option3": "Stakeholder expectations have been met",
+      "option4": "The customer has provided final payment",
+      "answer": 3
+  },
