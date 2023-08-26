e50d51fcc44cc6d816fbe5b68767a1763b4e0894
nbavaro1 <39228701+nbavaro1@users.noreply.github.com>
2023-08-26 11:55:11 -0400

    Update questionsCISSP.js

--- questionsCISSP.js
+++ questionsCISSP.js
@@ -402,6 +402,126 @@ var questions = [
       "option4": "Cross-site request forgery attack",
       "answer": 3
   },
+{ 
+      "question": "A CISSP must understand the different types of application updates. All updates should be obtained from the manufacturer only, and deployed into production only once tested on non-production systems. As such, what is the best answer that describes updates, patches, hot fixes, and service packs?   ",
+      "option1": "A hot fix has undergone full integration testing, has been released to address vulnerability, and addresses a specific issue; in most cases a hot fix is not appropriate for all systems. A security patch lacks full intergration and testing, has been released to address a vulnerability, and is mandatory. A service pack is a collection of patches that are critical and should be installed quickly",
+      "option2": "A hot fix is quick, lacks full integration and testing, and addresses a specific issue, in most cases a hot fix is not appropriate for all systems. A security patch is a collection of patches that are critical and has been rreleased to address a vulnerability. A service pack is a collection of patches and is considered critical.",
+      "option3": "A hot fix is a quick collection of critical install ASAP patches that address a specific issue. in most cases a hot fix is not appropriate for all systems. A security patch has undergone full integration testing, and has been released to address a vulnerability. A service pack is a collection of patches. ",
+      "option4": "A hot fix is slow and has full inegration and testing, and addresses a broad set of problems; in most cases a hot fix is appropriate for all systems. A security patch is a collection of patches that are not critical. A service pack is a collection of patches and is not considered critical.",
+      "answer": 2
+  },
+{ 
+      "question": "Which is the correct solution that describes the CIA triad when applied to software security?   ",
+      "option1": "Confidentiality prevents unauthorized access, integrity prevents unauthorized modification, and availability deals with countermeasures to prevent denial of service to authorized users.",
+      "option2": "Confidentiality prevents unauthorized modification, integrity prevent unauthorized access and avaliability deals with countermeasures to prevent denial of service to authorized users.",
+      "option3": "Confidentiality prevents unauthorized access, integrity prevents unauthorized modification, and availability deals with countermeasures to prevent unauthorized access.",
+      "option4": "Confidentiality deals with countermeasures to prevent denial of service to authorized users, integrity prevents unauthorized modification and availability prevents unauthorized access. ",
+      "answer": 1
+  },
+{ 
+      "question": "Which of the following tools can be used for change detection?  ",
+      "option1": "DES",
+      "option2": "Checksums",
+      "option3": "MD5sum",
+      "option4": "Parity bits",
+      "answer": 3
+  },
+{ 
+      "question": "Bob has noticed that when he inputs too much data into his new internet application, it momentarily locks up the computer and then halts the program. Which of the following best describes this situation?  ",
+      "option1": "Fail-safe",
+      "option2": "Buffer overflow",
+      "option3": "Fail-open",
+      "option4": "Fail-soft",
+      "answer": 4
+  },
+{ 
+      "question": "Which of the following types of database is considered a lattice structure, with each record having multiple parent and child records?  ",
+      "option1": "Hierarchical deatabase management system",
+      "option2": "Network database management system",
+      "option3": "Object oriented database managemment system",
+      "option4": "Relational database management system",
+      "answer": 2
+  },
+{ 
+      "question": "Which database term refers to the capability to restrict certain fields or rows from unauthorized individuals?  ",
+      "option1": "Low granularity",
+      "option2": "High resolution",
+      "option3": "High granularity",
+      "option4": "Low resolution",
+      "answer": 3
+  },
+{ 
+      "question": "Which of the following types of testing involves entering malformed, random data? ",
+      "option1": "XSS",
+      "option2": "Buffer overflow",
+      "option3": "Fuzzing",
+      "option4": "Whitebox testing",
+      "answer": 3
+  },
+{ 
+      "question": "OmniTecs new programmer has left several entry points in the new e-commerce shopping cart program for testing and development. Which of the following terms best describes whta the programmer has done?",
+      "option1": "Back door",
+      "option2": "Security flaw",
+      "option3": "SQL injection",
+      "option4": "Trapdoor",
+      "answer": 4
+  },
+{ 
+      "question": "Generation 2 programming languages are considered what?",
+      "option1": "Assembly",
+      "option2": "Machine",
+      "option3": "High level",
+      "option4": "Natural",
+      "answer": 1
+  },
+{ 
+      "question": "Which of the following is considered middleware?",
+      "option1": "Atomicity",
+      "option2": "OLE",
+      "option3": "CORBA",
+      "option4": "Object oriented programming",
+      "answer": 3
+  },
+{ 
+      "question": "After Debbie became the programmer for the new payroll application she placed some extra code int he application that would cause it to halt if she was fired and her name removed from payroll. What type of attack has she launched? ",
+      "option1": "Rounding down",
+      "option2": "Logic bomb",
+      "option3": "Salami",
+      "option4": "Buffer overflow",
+      "answer": 2
+  },
+{ 
+      "question": "While working on a penetration test assignment, you just descovered that the company's database driven ecommerce site will let you place a negative quantity into an order field so that the system will credit you money. What best describes this failure?",
+      "option1": "Referential integrity error",
+      "option2": "Buffer overflow",
+      "option3": "Semantic integrity error",
+      "option4": "Rounding down",
+      "answer": 3
+  },
+{ 
+      "question": "Which of the following best describes bytecode",
+      "option1": "Is processor specific",
+      "option2": "Is used with ActiveX",
+      "option3": "Is not processor specific",
+      "option4": "is used with COM and DCOM",
+      "answer": 3
+  },
+{ 
+      "question": "One of the best approaches to deal with attacks like SQL, LDAP and XML injection is what?",
+      "option1": "Using type safe languages",
+      "option2": "Manual review of code",
+      "option3": "Using emanations",
+      "option4": "Adequate parameter vaildation",
+      "answer": 4
+  },
+{ 
+      "question": "Which of the following program techniques would this phrase most closely be associated with Two objects may not know how the other object works and each is hidden from the other",
+      "option1": "Data modeling",
+      "option2": "Network database management system",
+      "option3": "Object oriented programming",
+      "option4": "Relational database management system",
+      "answer": 3
+  },
 
 
 
@@ -413,7 +533,13 @@ var questions = [
 
 
 
-  
+
+
+
+
+
+
+
   
   
 
